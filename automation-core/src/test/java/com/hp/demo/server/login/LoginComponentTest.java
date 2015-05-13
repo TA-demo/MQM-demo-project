88d5f55bd689f6eaf904a57e61e5cc1c77d6f191
Petr Dousa <petr.dousa@hp.com>
2015-05-13 15:04:55 +0200

    user story #4953 : initial structure + some simple tests

---
+++ automation-core/src/test/java/com/hp/demo/server/login/LoginComponentTest.java
@@ -0,0 +1,34 @@
+package com.hp.demo.server.login;
+
+import com.hp.demo.support.SystemTests;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(SystemTests.class)
+public class LoginComponentTest {
+    @Test
+    public void testDispatchCamera() { }
+
+    @Test
+    public void testNullModule() { }
+
+    @Test
+    public void testResetNegative() { }
+
+    @Test
+    public void testFoundContact() { }
+
+
+    @Test
+    public void testNullPerformance() { }
+
+    @Test
+    public void testFoundListener() { }
+
+    @Test
+    public void testRegistrationCallback() { }
+
+    @Test
+    public void testFoundPlatform() { }
+
+}
---
+++ automation-core/src/test/java/com/hp/demo/server/notifications/NotificationsHandlerTest.java
@@ -0,0 +1,33 @@
+package com.hp.demo.server.notifications;
+
+import com.hp.demo.support.UnitTests;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(UnitTests.class)
+public class NotificationsHandlerTest {
+
+    @Test
+    public void testSimpleModule() { }
+
+    @Test
+    public void testCreateAccount() { }
+
+    @Test
+    public void testInvalidImage() { }
+
+    @Test
+    public void testExportSearch() { }
+
+    @Test
+    public void testDelegateHandler() { }
+
+    @Test
+    public void testRegistrationHandler() { }
+
+    @Test
+    public void testMergeCallback() { }
+
+    @Test
+    public void testNullCamera() { }
+}
