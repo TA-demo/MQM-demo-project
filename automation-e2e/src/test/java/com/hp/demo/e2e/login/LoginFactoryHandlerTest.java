21e8ba67166ad2fa7d31ece4fd72385a8fc1cac9
Petr Dousa <petr.dousa@hp.com>
2015-05-13 16:29:37 +0200

    user story #4953 : adding test-ng support plus build profiles

---
+++ automation-e2e/src/test/java/com/hp/demo/e2e/login/LoginFactoryHandlerTest.java
@@ -0,0 +1,31 @@
+package com.hp.demo.e2e.login;
+
+import org.testng.annotations.Test;
+
+@Test(groups = { "com.hp.demo.support.FunctionalTests" })
+public class LoginFactoryHandlerTest {
+    @Test
+    public void testFoundLocation() { }
+
+    @Test
+    public void testCreateContact() { }
+
+    @Test
+    public void testMergeImage() { }
+
+    @Test
+    public void testFoundCamera() { }
+
+    @Test
+    public void testInvalidPerformance() { }
+
+    @Test
+    public void testCreateTasks() { }
+
+    @Test
+    public void testResetPlatform() { }
+
+    @Test
+    public void testExportNotifications() { }
+
+}
---
+++ automation-e2e/src/test/java/com/hp/demo/server/account/AccountComponentTest.java
@@ -0,0 +1,33 @@
+package com.hp.demo.server.account;
+
+import org.testng.annotations.Test;
+
+@Test(groups = "com.hp.demo.support.RESTTests")
+public class AccountComponentTest {
+    @Test
+    public void testSimpleLocation() { }
+
+    @Test
+    public void testFoundUseCase() { }
+
+    @Test
+    public void testMergeUtils() { }
+
+    @Test
+    public void testDelegateListener() { }
+
+    @Test
+    public void testSimpleCamera() { }
+
+    @Test
+    public void testCreateUseCase() { }
+
+    @Test
+    public void testCreateHandler() { }
+
+    @Test
+    public void testCreateUtils() { }
+
+    @Test
+    public void testDispatchCallback() { }
+}
